994349932d85129eab488632ce531b0d7262e21d
Edgar Robles <robles_edgar@icloud.com>
2024-11-25 14:07:26 -0600

    Grid update

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,15 +1,15 @@
 const Gallery = () => {
     return (
-        <div className="text-center w-full bg-[#ff3300]">
-            <div className="p-10 border border-white">
+        <div className="text-center m-4 sm:w-640 bg-[#ff3300]">
+            <div className="p-10 m-4 border border-white shadow">
                 <p className="text-4xl font-semibold">Brand Identity</p>
                 <p className="pt-2 text-sm">Build a strong brand identity that resonates with your customers.</p>
             </div>
-            <div className="p-10 border border-white">
+            <div className="p-10 m-4 border border-white">
                 <p className="text-4xl font-semibold">Social Media Campaign</p>
                 <p className="pt-2 text-sm">Engage your audience with creative and impactful social media campaigns.</p>
             </div>
-            <div className="p-10 border border-white">
+            <div className="p-10 m-4 border border-white">
                 <p className="text-4xl font-semibold">E-Commerce</p>
                 <p className="pt-2 text-sm">Start selling online with a custom e-commerce website.</p>
             </div>
