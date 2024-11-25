6a4d37b0ddc58f76a5dc0123704bb0e3200db5e8
Edgar Robles <robles_edgar@icloud.com>
2024-11-25 00:35:16 -0600

    Footer update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,14 @@ const Footer = () => {
   return (
     <footer className="bg-white">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm sm:text-center text-[#ff3300]">
+        <span className="text-xs sm:text-center text-[#ff3300]">
           © 2024{" "}
           <a href="#" className="hover:underline">
             ROBLES CREATIVE COMPANY
           </a>
           . All Rights Reserved.
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-400 sm:mt-0">
+        <ul className="flex flex-wrap items-center mt-3 text-xs font-medium text-gray-400 sm:mt-0">
           <li>
             <a href="#" className="hover:underline me-4 md:me-6">
               About
