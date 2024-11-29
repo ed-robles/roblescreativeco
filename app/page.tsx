118aec445fb57c42b1ac195e676685aab8b2bbf7
Edgar Robles <robles_edgar@icloud.com>
2024-11-29 13:41:58 -0600

    color overhaul

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,3 @@
-import Banner from "@/components/Banner";
-import BannerTwo from "@/components/BannerTwo";
-import Footer from "@/components/Footer";
-import Gallery from "@/components/Gallery";
-import Header from "@/components/Header";
-
 export default function Home() {
-  return (
-    <div className="flex flex-col min-h-svh ">
-      <Header />
-      <main className="grow bg-white">
-        <div className="flex flex-col items-center">
-          <Banner />
-        </div>
-        <div className="flex flex-col items-center">
-          <Gallery />
-        </div>
-        <div className="flex flex-col items-center">
-          <BannerTwo />
-        </div>
-      </main>
-      <Footer />
-    </div>
-  );
+  return <div>hello world</div>;
 }
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ const Banner = () => {
     <div className="text-gray-700 my-2 p-6">
       <p className="text-lg sm:w-[640px] text-center tracking-tight">
         At{" "}
-        <span className="text-[#ff3300] font-bold uppercase">
+        <span className="text-[#4FBBFF] font-bold uppercase">
           ROBLES CREATIVE COMPANY
         </span>
         , we empower businesses to thrive in the digital era by blending
